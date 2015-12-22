b475b7caf895b3e33883afff80dcf6509ff4b3a1
Colin Mitchell <muffinista@gmail.com>
2015-12-22 16:17:25 -0500

    updates to UI
    - add tray menu, definitely a work in progress
    - copy global.js from old project
    - add a 'Run Now' menu option and a way to open prefs screen

--- main.js
+++ main.js
@@ -2,6 +2,9 @@
 const electron = require('electron');
 const app = electron.app;  // Module to control application life.
 const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+const Menu = electron.Menu;
+const Tray = electron.Tray;
+
 
 require('./bootstrap.js');
 
@@ -34,25 +37,68 @@ app.on('window-all-closed', function() {
 });
 
 
+let appIcon = null;
+
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
-app.on('ready', function() {
-    var url = 'file://' + __dirname + '/ui/prefs.html';
-    windowOpts = {
-        width: 800, 
-        height: 600,
-        resizable: false
-    };
-    
-    var Menu = require('menu');
-    
+app.on('ready', function() {  
+    var prefsUrl = 'file://' + __dirname + '/ui/prefs.html';
+
     var openAddPackageWindow = function() {
         var w = new BrowserWindow({width:500, height:200});
         w.loadURL('file://' + __dirname + '/ui/add-new.html');
         w.on('closed', function() {
             w = null;
-            mainWindow.reload();
+            if ( mainWindow ) {
+                mainWindow.reload();
+            }
         });
+    };
+    
+    var openPrefsWindow = function() {
+        var w = new BrowserWindow({width:800, height:600});
+        w.loadURL(prefsUrl);
+        w.on('closed', function() {
+            w = null;
+            //mainWindow.reload();
+        });
+    };
+
+    var runScreenSaver = function() {
+        // lets pull together some handy values that screensavers might want to know about
+        // @todo different params for preview mode
+        var atomScreen = require('screen');
+        var size = atomScreen.getPrimaryDisplay().bounds;
+        var url_opts = { 
+            width: size.width,
+            height: size.height,
+            platform: process.platform
+        };
+        savers.setOpts(url_opts);
+
+        var url = savers.getCurrentUrl(url_opts);
+        console.log("loading " + url);
+        var windowOpts = {
+            fullscreen: true,
+            webgl: true,
+            preload: __dirname + '/global.js',
+            'auto-hide-menu-bar': true
+        };
+
+        // Create the browser window.
+        var saverWindow = new BrowserWindow(windowOpts);
+
+        // and load the index.html of the app.
+        saverWindow.loadUrl(url);
+
+        // Emitted when the window is closed.
+        saverWindow.on('closed', function() {
+            // Dereference the window object, usually you would store windows
+            // in an array if your app supports multi windows, this is the time
+            // when you should delete the corresponding element.
+            saverWindow = null;
+        });
+
     };
 
     var template = [
@@ -85,13 +131,44 @@ app.on('ready', function() {
         
     var menu = Menu.buildFromTemplate(template);
     Menu.setApplicationMenu(menu);
-    
+
+    var trayMenu = Menu.buildFromTemplate([
+        {
+            label: 'Run Now',
+            click: function() { runScreenSaver(); }
+        },
+
+        {
+            label: 'Add Package',
+            click: function() { openAddPackageWindow(); }
+        },
+        {
+            label: 'Preferences',
+            click: function() { openPrefsWindow(); }
+        },
+        {
+            label: 'Reload',
+            click: function() { mainWindow.reload(); }
+        },
+        {
+            label: 'Toggle DevTools',
+            click: function() { mainWindow.toggleDevTools(); }
+        },
+        {
+            label: 'Quit',
+            selector: 'terminate:'
+        }
+    ]);
+
+    appIcon = new Tray(__dirname + '/assets/icon.png');
+    appIcon.setToolTip("Before Dawn");
+    appIcon.setContextMenu(trayMenu); 
 
     // Create the browser window.
     mainWindow = new BrowserWindow(windowOpts);
 
     // and load the index.html of the app.
-    mainWindow.loadURL(url);
+    mainWindow.loadURL(prefsUrl);
     
     // Emitted when the window is closed.
     mainWindow.on('closed', function() {
