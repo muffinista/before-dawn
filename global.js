912f6f8fbd902cedf9263267ad5fcbd08c5fc755
Colin Mitchell <muffinista@gmail.com>
2016-01-12 09:42:34 -0500

    inject css into any screensavers. WIP

--- global.js
+++ global.js
@@ -4,3 +4,21 @@ window.urlParams = window.location.search.split(/[?&]/).slice(1).map(function(pa
     obj[pairArray[0]] = pairArray[1];
     return obj;
 }, {});
+
+var sheet = (function() {
+	// Create the <style> tag
+	var style = document.createElement("style");
+
+	// WebKit hack :(
+	style.appendChild(document.createTextNode(""));
+
+	// Add the <style> element to the page
+	document.head.appendChild(style);
+
+	return style.sheet;
+})();
+
+
+sheet.insertRule("body {margin:0; padding:0; cursor:none;}"); /* remove top and left whitespace */
+sheet.insertRule("canvas {display:block;}");    /* remove scrollbars */
+sheet.insertRule("canvas:focus {outline:0;}"); /* remove blue outline around canvas */
