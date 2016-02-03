8567c00ccf82dcf28930384688e2fef7361a84bf
Colin Mitchell <muffinista@gmail.com>
2016-02-02 19:38:10 -0500

    add some notes

--- global.js
+++ global.js
@@ -1,3 +1,8 @@
+/**
+ * parse any incoming URL parameters and put them into their own
+ * variable. depending on how/when you need to access these variables,
+ * there's a good chance that you are better off doing it yourself.
+ */
 window.urlParams = window.location.search.split(/[?&]/).slice(1).map(function(paramPair) {
     return paramPair.split(/=(.+)?/).slice(0, 2);
 }).reduce(function (obj, pairArray) {            
