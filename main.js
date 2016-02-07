a5ff879aecd4b3e76828e1c7946c19c846b6073b
Colin Mitchell <muffinista@gmail.com>
2016-02-07 13:33:27 -0500

    Add menu to prefs window
    The application doesn't really need a window but makes it possible to
    copy/paste text in OSX.
    
    @see https://github.com/atom/electron/issues/2402 for explanation of issue.

--- main.js
+++ main.js
@@ -52,9 +52,152 @@ var openPrefsWindow = function() {
             resizable:false,
             icon: __dirname + '/assets/icon.png'
         });
+
+        var menuTemplate = [
+            {
+                label: 'Edit',
+                submenu: [
+                    {
+                        label: 'Undo',
+                        accelerator: 'CmdOrCtrl+Z',
+                        role: 'undo'
+                    },
+                    {
+                        label: 'Redo',
+                        accelerator: 'Shift+CmdOrCtrl+Z',
+                        role: 'redo'
+                    },
+                    {
+                        type: 'separator'
+                    },
+                    {
+                        label: 'Cut',
+                        accelerator: 'CmdOrCtrl+X',
+                        role: 'cut'
+                    },
+                    {
+                        label: 'Copy',
+                        accelerator: 'CmdOrCtrl+C',
+                        role: 'copy'
+                    },
+                    {
+                        label: 'Paste',
+                        accelerator: 'CmdOrCtrl+V',
+                        role: 'paste'
+                    },
+                    {
+                        label: 'Select All',
+                        accelerator: 'CmdOrCtrl+A',
+                        role: 'selectall'
+                    }
+                ]
+            },
+            {
+                label: 'View',
+                submenu: [
+                    {
+                        label: 'Reload',
+                        accelerator: 'CmdOrCtrl+R',
+                        click: function(item, focusedWindow) {
+                            if (focusedWindow)
+                                focusedWindow.reload();
+                        }
+                    },
+                    {
+                        label: 'Toggle Developer Tools',
+                        accelerator: (function() {
+                            if (process.platform == 'darwin')
+                                return 'Alt+Command+I';
+                            else
+                                return 'Ctrl+Shift+I';
+                        })(),
+                        click: function(item, focusedWindow) {
+                            if (focusedWindow)
+                                focusedWindow.toggleDevTools();
+                        }
+                    }
+                ]
+            },
+            {
+                label: 'Window',
+                role: 'window',
+                submenu: [
+                    {
+                        label: 'Minimize',
+                        accelerator: 'CmdOrCtrl+M',
+                        role: 'minimize'
+                    },
+                    {
+                        label: 'Close',
+                        accelerator: 'CmdOrCtrl+W',
+                        role: 'close'
+                    }
+                ]
+            },
+            {
+                label: 'Help',
+                role: 'help',
+                submenu: [
+                    {
+                        label: 'Learn More',
+                        click: function() {
+                            require('electron').shell.openExternal('https://github.com/muffinista/before-dawn');
+                        }
+                    }
+                ]
+            }
+        ];
+
+        if (process.platform == 'darwin') {
+            var name = require('electron').app.getName();
+            menuTemplate.unshift({
+                label: name,
+                submenu: [
+                    {
+                        label: 'About ' + name,
+                        role: 'about'
+                    },
+                    {
+                        type: 'separator'
+                    },
+                    {
+                        label: 'Services',
+                        role: 'services',
+                        submenu: []
+                    },
+                    {
+                        type: 'separator'
+                    },
+                    {
+                        label: 'Hide ' + name,
+                        accelerator: 'Command+H',
+                        role: 'hide'
+                    },
+                    {
+                        label: 'Hide Others',
+                        accelerator: 'Command+Alt+H',
+                        role: 'hideothers'
+                    },
+                    {
+                        label: 'Show All',
+                        role: 'unhide'
+                    },
+                    {
+                        type: 'separator'
+                    },
+                    {
+                        label: 'Quit',
+                        accelerator: 'Command+Q',
+                        click: function() { app.quit(); }
+                    }
+                ]
+            });
+        }
+
+        var menu = Menu.buildFromTemplate(menuTemplate);
+        Menu.setApplicationMenu(menu);
         
         w.loadURL(prefsUrl);
-        w.setMenu(null);
 
         if ( typeof(app.dock) !== "undefined" ) {
             app.dock.show();
