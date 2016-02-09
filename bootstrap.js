e08857a16ab220ad60cf039363b3e7e039f14361
Colin Mitchell <muffinista@gmail.com>
2016-02-09 16:51:44 -0500

    version bump

--- bootstrap.js
+++ bootstrap.js
@@ -1,5 +1,5 @@
 // store our root path as a global variable
 global.APP_NAME = "Before Dawn";
 global.SAVER_REPO = "muffinista/before-dawn-screensavers";
-global.APP_VERSION = "v0.1";
+global.APP_VERSION = "v0.3";
 
