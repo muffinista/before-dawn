a9008145e0e918e2f9ff9afcf634b41adb90485c
Colin Mitchell <muffinista@gmail.com>
2016-02-12 22:53:29 -0500

    Remove code to generate urlParams object. I don't want people to rely on this

--- global.js
+++ global.js
@@ -1,15 +1,3 @@
-/**
- * parse any incoming URL parameters and put them into their own
- * variable. depending on how/when you need to access these variables,
- * there's a good chance that you are better off doing it yourself.
- */
-window.urlParams = window.location.search.split(/[?&]/).slice(1).map(function(paramPair) {
-    return paramPair.split(/=(.+)?/).slice(0, 2);
-}).reduce(function (obj, pairArray) {            
-    obj[pairArray[0]] = pairArray[1];
-    return obj;
-}, {});
-
 // waiting for a bit before hiding the cursor seems to help in windows
 // @see http://delphic.me.uk/controllingthecursor.html for cursor url idea
 var hideCursor = function() {
