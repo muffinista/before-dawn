bbd4e3136824620d2486a133884d3ebb14f8329e
Colin Mitchell <muffinista@gmail.com>
2016-02-20 11:27:32 -0500

    Add logic to only skip first mouse event. Add some more key events

--- global-js-handlers.js
+++ global-js-handlers.js
@@ -1,11 +1,11 @@
 var firstMouseMove = false;
 
-var exitScreenSaver = function() {
+var exitScreenSaver = function(e) {
     var ipcRenderer;
 
     // seems like you get a mousemove event even without moving the mouse, i'm
     // guessing to establish initial position/etc. anyway, we can skip that event
-    if ( firstMouseMove === false ) {
+    if ( e.type.indexOf("mouse") !== -1 && firstMouseMove === false ) {
         firstMouseMove = true;
         return;
     }
@@ -14,7 +14,7 @@ var exitScreenSaver = function() {
 };
                 
 var body = document.getElementsByTagName("body")[0];
-var listenEvents = ["mousedown", "mousemove", "mousewheel", "keydown"];
+var listenEvents = ["mousedown", "mousemove", "mousewheel", "keydown", "keyup", "keypress"];
 
 for ( var i = 0; i < listenEvents.length; i++ ) {
     var name = listenEvents[i];
