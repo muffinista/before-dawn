fd25d77bb030bd4d7d5f2f36afe9a5df8ca5d931
Colin Mitchell <muffinista@gmail.com>
2016-02-20 14:28:42 -0500

    super-rough linux setup notes

---
+++ linux-setup.sh
@@ -0,0 +1,16 @@
+ curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+    9  sudo apt-get install -y nodejs
+   10  npm i -g grunt-cli
+   11  sudo npm i -g grunt-cli
+
+
+sudo apt-get install libx11-dev libxss-dev
+
+sudo apt-get install libappindicator1
+
+
+sudo apt-get install ruby-dev
+   46  sudo gem install fpm
+   47  gem
+   48  sudo apt-get install ruby
+   49  sudo gem install fpm
