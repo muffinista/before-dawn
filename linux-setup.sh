e0d175ad05e06185bf20daff5b0a74a85253d359
Colin Mitchell <muffinista@gmail.com>
2016-02-21 11:50:44 -0500

    Work on linux build

--- linux-setup.sh
+++ linux-setup.sh
@@ -1,16 +1,11 @@
- curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
-    9  sudo apt-get install -y nodejs
-   10  npm i -g grunt-cli
-   11  sudo npm i -g grunt-cli
+#!/bin/bash
 
+curl -sL https://deb.nodesource.com/setup_4.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+sudo npm i -g grunt-cli electron-packager electron-builder
 
 sudo apt-get install libx11-dev libxss-dev
-
 sudo apt-get install libappindicator1
-
-
-sudo apt-get install ruby-dev
-   46  sudo gem install fpm
-   47  gem
-   48  sudo apt-get install ruby
-   49  sudo gem install fpm
+sudo apt-get install ruby ruby-dev
+sudo gem install fpm
