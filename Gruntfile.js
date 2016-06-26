ff7722818a215c9031ddefac1fbd9873a711b0cb
Colin Mitchell <muffinista@gmail.com>
2016-06-26 12:36:33 -0400

    Simple tab handler implementation

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,20 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-      babel: {
-          options: {
-              sourceMap: true,
-              presets: ['es2015', 'react']
-          },
-          dist: {
-              files: {
-                  'ui/prefs.js': 'ui/prefs.jsx'
-              }
-          }
+    babel: {
+      options: {
+        sourceMap: true,
+        presets: ['es2015', 'react']
+      },
+      dist: {
+        files: {
+          'ui/prefs.js': 'ui/prefs.jsx'
+        }
       }
+    }
   });
-
-    grunt.loadNpmTasks('grunt-babel');
-    grunt.loadNpmTasks('load-grunt-tasks');
-    grunt.registerTask('default', ['babel']);
+  
+  grunt.loadNpmTasks('grunt-babel');
+  grunt.loadNpmTasks('load-grunt-tasks');
+  grunt.registerTask('default', ['babel']);
 };
