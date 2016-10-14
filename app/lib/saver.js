0fe9feefaecf6ca35eef453f7334fff8452ee404
Colin Mitchell <muffinista@gmail.com>
2016-10-14 11:49:29 -0400

    Add toHash and write to manipulate saver as Hash/JSON

--- app/lib/saver.js
+++ app/lib/saver.js
@@ -18,34 +18,37 @@ var serialize = function(obj) {
 };
 
 module.exports = function Saver(_attrs) {
-    this.attrs = _attrs;
-    this.path = _attrs.path;
-    this.name = _attrs.name;
-    this.key = _attrs.key;
-    this.description = _attrs.description;
-    this.aboutUrl = _attrs.aboutUrl;
-    this.author = _attrs.author;
-    this.license = _attrs.license;
-    this.url = _attrs.url;
+  this.UNWRITABLE_KEYS = ["key", "path", "url", "settings"];
 
-    this.published = _attrs.published;
-    if ( typeof(this.published) === "undefined" ) {
-        this.published = true;
+
+  this.attrs = _attrs;
+  this.path = _attrs.path;
+  this.name = _attrs.name;
+  this.key = _attrs.key;
+  this.description = _attrs.description;
+  this.aboutUrl = _attrs.aboutUrl;
+  this.author = _attrs.author;
+  this.license = _attrs.license;
+  this.url = _attrs.url;
+
+  this.published = _attrs.published;
+  if ( typeof(this.published) === "undefined" ) {
+    this.published = true;
+  }
+
+  this.valid = typeof(this.name) !== "undefined" &&
+               typeof(this.description) !== "undefined" &&
+               this.published === true;
+
+  if ( this.valid === true ) {
+    if ( typeof(_attrs.options) === "undefined" ) {
+      _attrs.options = [];
     }
+    this.options = _attrs.options;
 
-    this.valid = typeof(this.name) !== "undefined" &&
-        typeof(this.description) !== "undefined" &&
-        this.published === true;
-
-    if ( this.valid === true ) {
-        if ( typeof(_attrs.options) === "undefined" ) {
-            _attrs.options = [];
-        }
-        this.options = _attrs.options;
-
-        // figure out the settings from any defaults for this screensaver,
-        // and combine with incoming user-specified settings
-        this.settings = _attrs.options.map(function(o, i) {
+    // figure out the settings from any defaults for this screensaver,
+    // and combine with incoming user-specified settings
+    this.settings = _attrs.options.map(function(o, i) {
             return [o.name, o.default];
         }).reduce(function(o, v, i) {
             o[v[0]] = v[1];
@@ -96,5 +99,28 @@ module.exports = function Saver(_attrs) {
         opts = serialize(opts);
         return url + joiner + opts;
     };
+
+  this.toHash = function() {
+    return this.attrs;   
+  };
+
+  // write a new set of attributes for this saver to it's JSON file
+  this.write = function(attrs) {
+    var _path = require('path');
+    var fs = require('fs');
+
+    var configDest = _path.join(this.path, "saver.json");
+
+    for ( var i = 0 ; i < this.UNWRITABLE_KEYS.length; i++ ) {
+      delete(attrs[this.UNWRITABLE_KEYS[i]]);
+    }
+    
+    var output = JSON.stringify(attrs, null, 2);
+    console.log(output);
+    console.log("write to " + configDest);
+    
+    fs.writeFileSync(configDest, output);
+
+  }
 };
 
