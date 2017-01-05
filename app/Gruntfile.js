e8d94d83d8f78d5407aca3f3609b68b375ffb667
Colin Mitchell <muffinista@gmail.com>
2017-01-05 10:40:24 -0500

    Update grunt setup to compile JSX properly

--- app/Gruntfile.js
+++ app/Gruntfile.js
@@ -11,7 +11,7 @@ module.exports = function(grunt) {
           {
             expand: true,
             cwd: 'ui/js',
-            src: ['*.js'],
+            src: ['*.js', '*.jsx'],
             ext: '.js',
             dest: 'ui/'
           }
