7eb051c63c23effb2a7fa7961c5b3aed3edd07ab
Colin Mitchell <muffinista@gmail.com>
2017-04-26 14:18:41 -0400

    Remove unused saver window event checking

--- app/assets/global-js-handlers.js
+++ app/assets/global-js-handlers.js
@@ -1,33 +1,3 @@
-var firstMouseMove = false;
-
-/**
- * send a message that the user has done something and we should close
-   the screensaver
- */
-var exitScreenSaver = function(e) {
-  var ipcRenderer;
-
-  // seems like you get a mousemove event even without moving the mouse, i'm
-  // guessing to establish initial position/etc. anyway, we can skip that event
-  if ( e.type.indexOf("mouse") !== -1 && firstMouseMove === false ) {
-    firstMouseMove = true;
-    return;
-  }
-  ipcRenderer = require('electron').ipcRenderer;
-  ipcRenderer.send('stopScreenSaver');
-};
-
-
-var body = document.getElementsByTagName("body")[0];
-
-var listenEvents = ["mousedown", "mousemove", "mousewheel", "keydown", "keyup", "keypress"];
-
-for ( var i = 0; i < listenEvents.length; i++ ) {
-  var name = listenEvents[i];
-  body.addEventListener(name, exitScreenSaver, false);
-}
-
-
 window.hideCursor = function() {
   var cursorUrl = "url('data:image/gif;base64,R0lGODlhAQABAPAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==')";
 
