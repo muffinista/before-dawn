c845bc868930529675585327b2e6550eebe89b85
Colin Mitchell <muffinista@gmail.com>
2017-10-13 22:04:28 -0400

    Restore debugging logging

--- app/bootstrap.js
+++ app/bootstrap.js
@@ -14,9 +14,9 @@ global.HELP_URL = "https://muffinista.github.io/before-dawn/";
 global.ISSUES_URL = "https://github.com/muffinista/before-dawn/issues";
 
 // this is a free sentry account and the URL will be in every copy of
-// the app that gets distrubuted, so i'm committing it to the repo for now
-//global.RAVEN_PRIVATE_URL = 'https://b86f7b0ac5604b55b4fd03adedc5d205:9cc446fadc234baab6d825e88fe4215d@sentry.io/172824';
-//global.RAVEN_URL = 'https://b86f7b0ac5604b55b4fd03adedc5d205@sentry.io/172824';
+// the app that gets distributed, so i'm committing it to the repo for now
+global.RAVEN_PRIVATE_URL = 'https://b86f7b0ac5604b55b4fd03adedc5d205:9cc446fadc234baab6d825e88fe4215d@sentry.io/172824';
+global.RAVEN_URL = 'https://b86f7b0ac5604b55b4fd03adedc5d205@sentry.io/172824';
 
 global.CRASH_REPORTER = {
   productName: "Before Dawn",
