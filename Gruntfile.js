07041cff986a57910908d8ba1b8726d1343af113
Colin Mitchell <muffinista@gmail.com>
2017-10-18 16:59:36 -0400

    Cleanup

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,44 +2,33 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      all: ['Gruntfile.js', 'app/**/*.js', 'test/**/*.js', '!node_modules/**', '!app/node_modules/**', '!app/ui/*.js'],
-      options: {
-        jshintrc: ".jshintrc"
-      }
-    },
-    simplemocha: {
-      options: {
-        globals: ['expect'],
-        timeout: 3000,
-        ignoreLeaks: false
-      },
-      all: { src: ['test/**/*.js'] }
-    },
     mochaTest: {
+      options: {
+        reporter: 'spec',
+        captureFile: 'results.txt', // Optionally capture the reporter output to a file
+        quiet: false, // Optionally suppress output to standard out (defaults to false)
+        clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
+        clearCacheFilter: (key) => true, // Optionally defines which files should keep in cache
+        noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
+      },
       test: {
-        options: {
-          reporter: 'spec',
-          captureFile: 'results.txt', // Optionally capture the reporter output to a file
-          quiet: false, // Optionally suppress output to standard out (defaults to false)
-          clearRequireCache: false, // Optionally clear the require cache before running tests (defaults to false)
-          clearCacheFilter: (key) => true, // Optionally defines which files should keep in cache
-          noFail: false // Optionally set to not fail on failed tests (will still fail on other errors)
-        },
         src: ['test/**/*.js']
+      },
+      lib: {
+        src: ['test/lib/*.js']
       }
+      
     },
     watch: {
       options: { interval: 1000 },
       scripts: {
         files: ['gruntfile.js', 'app/*.js', 'app/**/*.js', 'test/**/*.js'],
-        tasks: ['jshint', 'mochaTest']
+        tasks: ['mochaTest']
       }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
-  //  grunt.loadNpmTasks('grunt-simple-mocha');
   grunt.loadNpmTasks('grunt-mocha-test');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
@@ -54,6 +43,6 @@ module.exports = function(grunt) {
   });
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'mochaLite']);
+  grunt.registerTask('default', ['mochaTest']);
   grunt.registerTask('mocha', ['mochaTest']);  
 };
