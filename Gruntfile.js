bee1cb5e3b4e13db892d826fa017288a227e30a3
Colin Mitchell <muffinista@gmail.com>
2017-11-01 11:44:58 -0400

    Remove some unneeded packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -77,7 +77,6 @@ module.exports = function(grunt) {
   });
 
 
-  grunt.loadNpmTasks("grunt-contrib-jshint");
   grunt.loadNpmTasks("grunt-mocha-test");
   grunt.loadNpmTasks("grunt-contrib-watch");
   grunt.loadNpmTasks("gruntify-eslint");
