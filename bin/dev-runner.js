7022d34038e2665619e96ef4d8562f6333d7d1f5
Colin Mitchell <muffinista@gmail.com>
2017-11-30 15:31:03 -0500

    Cleanup output

--- bin/dev-runner.js
+++ bin/dev-runner.js
@@ -1,9 +1,7 @@
 'use strict'
 
-const chalk = require('chalk')
 const electron = require('electron')
 const path = require('path')
-const { say } = require('cfonts')
 const { spawn } = require('child_process')
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
@@ -19,7 +17,7 @@ let hotMiddleware
 function logStats (proc, data) {
   let log = ''
 
-  log += chalk.yellow.bold(`┏ ${proc} Process ${new Array((19 - proc.length) + 1).join('-')}`)
+  log += `┏ ${proc} Process ${new Array((19 - proc.length) + 1).join('-')}`;
   log += '\n\n'
 
   if (typeof data === 'object') {
@@ -33,7 +31,7 @@ function logStats (proc, data) {
     log += `  ${data}\n`
   }
 
-  log += '\n' + chalk.yellow.bold(`┗ ${new Array(28 + 1).join('-')}`) + '\n'
+  log += '\n' + `┗ ${new Array(28 + 1).join('-')}` + '\n';
 
   console.log(log)
 }
@@ -77,96 +75,24 @@ function startRenderer () {
   })
 }
 
-/*function startMain () {
-  return new Promise((resolve, reject) => {
-    mainConfig.entry.main = [path.join(__dirname, '../app/main/index.dev.js')].concat(mainConfig.entry.main)
-
-    const compiler = webpack(mainConfig)
-
-    compiler.plugin('watch-run', (compilation, done) => {
-      logStats('Main', chalk.white.bold('compiling...'))
-      hotMiddleware.publish({ action: 'compiling' })
-      done()
-    })
-
-    compiler.watch({}, (err, stats) => {
-      if (err) {
-        console.log(err)
-        return
-      }
-
-      logStats('Main', stats)
-
-      if (electronProcess && electronProcess.kill) {
-        manualRestart = true
-        process.kill(electronProcess.pid)
-        electronProcess = null
-        startElectron()
-
-        setTimeout(() => {
-          manualRestart = false
-        }, 5000)
-      }
-
-      resolve()
-    })
-  })
-}*/
-
 function startElectron () {
   electronProcess = spawn(electron, ['--inspect=5858', path.join(__dirname, '../app/main/index.js')])
 
   electronProcess.stdout.on('data', data => {
-    electronLog(data, 'blue')
+    process.stdout.write(data.toString());
   })
   electronProcess.stderr.on('data', data => {
-    electronLog(data, 'red')
+    process.stdout.write(data.toString());
   })
-
   electronProcess.on('close', () => {
     if (!manualRestart) process.exit()
   })
 }
 
-function electronLog (data, color) {
-  let log = ''
-  data = data.toString().split(/\r?\n/)
-  data.forEach(line => {
-    log += `  ${line}\n`
-  })
-  if (/[0-9A-z]+/.test(log)) {
-    console.log(
-      chalk[color].bold('┏ Electron -------------------') +
-      '\n\n' +
-      log +
-      chalk[color].bold('┗ ----------------------------') +
-      '\n'
-    )
-  }
-}
-
-function greeting () {
-  const cols = process.stdout.columns
-  let text = ''
-
-  if (cols > 104) text = 'electron-vue'
-  else if (cols > 76) text = 'electron-|vue'
-  else text = false
-
-  if (text) {
-    say(text, {
-      colors: ['yellow'],
-      font: 'simple3d',
-      space: false
-    })
-  } else console.log(chalk.yellow.bold('\n  electron-vue'))
-  console.log(chalk.blue('  getting ready...') + '\n')
-}
-
 function init () {
-  greeting()
+  console.log("booting up!");
 
-  Promise.all([startRenderer()]) // , startMain()
+  Promise.all([startRenderer()])
     .then(() => {
       startElectron()
     })
