88773bc1c64e8f3262a11a4aac48dbdf6c201a33
Colin Mitchell <muffinista@gmail.com>
2017-12-01 13:07:25 -0500

    Remove some unused (i think) __static code

--- src/main/index.js
+++ src/main/index.js
@@ -75,14 +75,6 @@ var icons = {
 };
 
 
-/**
- * Set `__static` path to static files in production
- * https://simulatedgreg.gitbooks.io/electron-vue/content/en/using-static-assets.html
- */
-if (process.env.NODE_ENV !== 'development') {
-  global.__static = require('path').join(__dirname, '/static').replace(/\\/g, '\\\\') // eslint-disable-line
-}
-
 const urlPrefix = process.env.NODE_ENV === 'development'
              ? 'http://localhost:9080'
              : 'file://' + __dirname
