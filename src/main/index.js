b62c04cf09bc19030522afb413fa5c4bf7341093
Colin Mitchell <muffinista@gmail.com>
2017-12-06 14:20:02 -0500

    Move menu template into main module

--- src/main/index.js
+++ src/main/index.js
@@ -618,9 +618,7 @@ var openPrefsOnFirstLoad = function() {
  */
 var bootApp = function() {
   var icons = getIcons();
-  // @todo i think asar breaks this
-  var menuTemplate = require("./menu_template.js").buildMenuTemplate(app);
-  var menu = Menu.buildFromTemplate(menuTemplate);
+  var menu = Menu.buildFromTemplate(buildMenuTemplate(app));
 
   Menu.setApplicationMenu(menu);
 
@@ -823,8 +821,185 @@ var updateTrayIcon = function() {
   }
 };
 
+var buildMenuTemplate = function(a) {
+  var app = a;
+  var openAddNewWindow = function(w) {
+    w.webContents.send("request-open-add-screensaver");
+  };
+
+  var base = [
+    {
+      label: "File",
+      submenu: [
+        {
+          label: "Add New Screensaver",
+          accelerator: "CmdOrCtrl+N",
+          click: function(item, focusedWindow) {
+            openAddNewWindow(focusedWindow);
+          }
+        },
+      ]
+    },
+
+    {
+      label: "Edit",
+      submenu: [
+        {
+          label: "Undo",
+          accelerator: "CmdOrCtrl+Z",
+          role: "undo"
+        },
+        {
+          label: "Redo",
+          accelerator: "Shift+CmdOrCtrl+Z",
+          role: "redo"
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Cut",
+          accelerator: "CmdOrCtrl+X",
+          role: "cut"
+        },
+        {
+          label: "Copy",
+          accelerator: "CmdOrCtrl+C",
+          role: "copy"
+        },
+        {
+          label: "Paste",
+          accelerator: "CmdOrCtrl+V",
+          role: "paste"
+        },
+        {
+          label: "Select All",
+          accelerator: "CmdOrCtrl+A",
+          role: "selectall"
+        }
+      ]
+    },
+    {
+      label: "View",
+      submenu: [
+        {
+          label: "Reload",
+          accelerator: "CmdOrCtrl+R",
+          click: function(item, focusedWindow) {
+            if (focusedWindow) {
+              focusedWindow.reload();
+            }
+          }
+        },
+        {
+          label: "Toggle Developer Tools",
+          accelerator: (function() {
+            if (process.platform == "darwin") {
+              return "Alt+Command+I";
+            }
+            else {
+              return "Ctrl+Shift+I";
+            }
+          })(),
+          click: function(item, focusedWindow) {
+            if (focusedWindow) {
+              focusedWindow.toggleDevTools();
+            }
+          }
+        }
+      ]
+    },
+    {
+      label: "Window",
+      role: "window",
+      submenu: [
+        {
+          label: "Minimize",
+          accelerator: "CmdOrCtrl+M",
+          role: "minimize"
+        },
+        {
+          label: "Close",
+          accelerator: "CmdOrCtrl+W",
+          role: "close"
+        }
+      ]
+    },
+    {
+      label: "Help",
+      role: "help",
+      submenu: [
+        {
+          label: "Learn More",
+          click: function() {
+            require("electron").shell.openExternal("https://github.com/muffinista/before-dawn");
+          }
+        },
+        {
+          label: "Help",
+          click: function() {
+            require("electron").shell.openExternal(global.HELP_URL);
+          }
+        }
+      ]
+    }
+  ];
+
+
+  if (process.platform == "darwin") {
+    var name = app.getName();
+    base.unshift({
+      label: name,
+      submenu: [
+        {
+          label: "About " + name,
+          role: "about"
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Services",
+          role: "services",
+          submenu: []
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Hide " + name,
+          accelerator: "Command+H",
+          role: "hide"
+        },
+        {
+          label: "Hide Others",
+          accelerator: "Command+Alt+H",
+          role: "hideothers"
+        },
+        {
+          label: "Show All",
+          role: "unhide"
+        },
+        {
+          type: "separator"
+        },
+        {
+          label: "Quit",
+          accelerator: "Command+Q",
+          click: function() { app.quit(); }
+        }
+      ]
+    });
+  }
+
+
+  return base;
+};
+
+
 // load a few global variables
 require("./bootstrap.js");
+
 
 log.transports.file.maxSize = 1 * 1024 * 1024;
 log.transports.file.file = path.join(__dirname, "log.txt");
