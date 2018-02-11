c1af45665b41546083ec14cbfb93c80cc9c7ccb4
Colin Mitchell <muffinista@gmail.com>
2018-02-10 22:28:15 -0500

    Add UI for setting up boolean inputs

--- src/renderer/components/OptionInput.vue
+++ src/renderer/components/OptionInput.vue
@@ -2,35 +2,14 @@
   <div id="wrapper">
     <form v-on="$listeners">
       <label>{{option.description}}</label>
-      <template v-if="option.type === 'boolean'">
-        <label>Yes
-          <input
-            type="radio"
-            :name="option.name"
-            v-model="value"
-            v-bind:value="true"
-            :saver="saver"
-            v-on:change="emitChange(saver, option.name, $event.target.value)"></label>
-        
-        <label>No
-          <input
-            type="radio"
-            :name="option.name"
-            v-model="value"
-            v-bind:value="false"
-            :saver="saver"
-            v-on:change="emitChange(saver, option.name, $event.target.value)"></label>
-      </template>
-      <template v-if="option.type !== 'boolean'">
-        <input
-          :type="renderType"
-          :name="option.name"
-          :min="option.min"
-          :max="option.max"
-          :value="value"
-          :saver="saver"
-          v-on:change="emitChange(saver, option.name, $event.target.value)" />
-      </template>
+      <input
+        :type="renderType"
+        :name="option.name"
+        :min="option.min"
+        :max="option.max"
+        :value="value"
+        :saver="saver"
+        v-on:change="emitChange(saver, option.name, $event.target.value)" />
     </form>
   </div>
 </template>
@@ -42,9 +21,6 @@
     components: { },
     computed: {
       renderType: function() {
-        if ( this.option.type === "boolean" ) {
-          return "checkbox";
-        }
         if ( this.option.type === "slider" ) {
           return "range";
         }
--- src/renderer/components/SaverOptionInput.vue
+++ src/renderer/components/SaverOptionInput.vue
@@ -31,34 +31,60 @@
                 class="form-control">
           <option value="slider">slider</option>
           <option value="text">text</option>
+          <option value="boolean">yes/no</option>
         </select>
       </div>
     </div>
 
-    <div class="form-group row">
-      <label class="col-sm-2 col-form-label only-for-slider">Min</label>
-      <div class="col-sm-2 only-for-slider">
-        <input type="number"
-               v-model="option.min"
-               class="form-control" />
-      </div>
+    <template v-if="option.type === 'slider'">
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label only-for-slider">Min</label>
+        <div class="col-sm-2 only-for-slider">
+          <input type="number"
+                 v-model="option.min"
+                 class="form-control" />
+        </div>
+        
+        <label class="col-sm-2 col-form-label only-for-slider">Max</label>
+        <div class="col-sm-2 only-for-slider">
+          <input type="number"
+                 v-model="option.max"
+                 class="form-control" />
+        </div>
       
-      <label class="col-sm-2 col-form-label only-for-slider">Max</label>
-      <div class="col-sm-2 only-for-slider">
-        <input type="number"
-               v-model="option.max"
-               class="form-control" />
+        <label class="col-sm-2 col-form-label">Default</label>
+        <div :class="defaultInputClass">
+          <input type="text"
+                 v-model="option.default"
+                 placeholder="Default value of this option"
+                 class="form-control" />
+        </div>
       </div>
-      
-      <label class="col-sm-2 col-form-label">Default</label>
-      <div :class="defaultInputClass">
-        <input type="text"
-               v-model="option.default"
-               placeholder="Default value of this option"
-               class="form-control" />
+    </template>
+    <template v-if="option.type === 'text'">
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Default</label>
+        <div :class="defaultInputClass">
+          <input type="text"
+                 v-model="option.default"
+                 placeholder="Default value of this option"
+                 class="form-control" />
+        </div>
       </div>
-    </div>
-    
+    </template>
+
+    <template v-if="option.type ==='boolean'">
+      <div class="form-group row">
+        <label class="col-sm-2 col-form-label">Default</label>
+        <div :class="defaultInputClass">
+          <select v-model="option.default" class="form-control">
+            <option disabled value="">Please select one</option>
+            <option value="true">Yes</option>
+            <option value="false">No</option>
+          </select>
+        </div>
+      </div>
+    </template>
     <div class="form-actions">
       <button type="button"
               class="btn btn-danger remove-option"
@@ -76,6 +102,9 @@
       formType: function() {
         if ( this.option.type === "slider" ) {
           return "entry option-range";
+        }
+        else if ( this.option.type === "boolean" ) {
+          return "entry option-boolean";
         }
         return "entry option-text";
       },
