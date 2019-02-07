2bf63ee1855c77b6d0932ef074be670225341313
Colin Mitchell <colin@muffinlabs.com>
2019-02-07 18:25:35 -0500

    Add hacky script for generating new release

--- bin/create-sentry-release.sh
+++ bin/create-sentry-release.sh
@@ -7,18 +7,18 @@ VERSION=$(bin/get-release-name)
 echo "VERSION: $VERSION"
 
 # Create a release
-sentry-cli releases new $VERSION
+sentry-cli releases new "$VERSION"
 
 # Associate commits with the release
-sentry-cli releases set-commits --auto $VERSION
+sentry-cli releases set-commits --auto "$VERSION"
 
 # upload source maps
-sentry-cli releases files $VERSION upload-sourcemaps output
+sentry-cli releases files "$VERSION" upload-sourcemaps output
 
-sentry-cli releases finalize $VERSION
+sentry-cli releases finalize "$VERSION"
 
 # notify about new deploy
-sentry-cli releases deploys $VERSION new --env production
+sentry-cli releases deploys "$VERSION" new --env production
 
 # upload symbols
 bin/sentry-symbols.js
