450cb579083bad2828a3acd431d7694e9d9973f9
Colin Mitchell <colin@muffinlabs.com>
2019-06-14 09:39:39 -0400

    Experiment with CI testing some more

--- test/ui/settings.js
+++ test/ui/settings.js
@@ -10,23 +10,21 @@ const SaverPrefs = require("../../src/lib/prefs.js");
 
 describe("Settings", function() { 
   const fakeDialogOpts = [ { method: "showOpenDialog", value: ["/not/a/real/path"] } ];
+  const closeWindowDelay = 750;
 
-  before(function() {
-    if ( process.env.CI ) {
-      // eslint-disable-next-line no-console
-      console.log("Cowardly skipping test in CI");
-      this.skip();
-      return;
-    }
-  });
-
+  // before(function() {
+  //   if ( process.env.CI ) {
+  //     // eslint-disable-next-line no-console
+  //     console.log("Cowardly skipping test in CI");
+  //     this.skip();
+  //     return;
+  //   }
+  // });
 
   helpers.setupTimeout(this);
-
-  // retry all tests in this suite up to 3 times
   helpers.setupRetries(this);
 
-  let pickPrefsWindow = function() {
+  let pickSettingsWindow = function() {
     return helpers.getWindowByTitle(app, "Before Dawn: Settings");
   };
 
@@ -57,7 +55,7 @@ describe("Settings", function() {
 	});
 
   it("set general preferences", function() {
-    return pickPrefsWindow().
+    return pickSettingsWindow().
       then(() => app.client.waitUntilTextExists("body", "Activate after")).
       then(() => 
         app.client.selectByVisibleText("[name=delay]", "30 minutes")
@@ -66,69 +64,72 @@ describe("Settings", function() {
         app.client.selectByVisibleText("[name=\"sleep\"]", "15 minutes")
       ).
       then(() => app.client.click("button.save")).
-      then(() => helpers.sleep(100)).
+      then(() => helpers.sleep(closeWindowDelay)).
       then(function() {
-        assert.equal(30, currentPrefs().delay);
-        assert.equal(15, currentPrefs().sleep);
-      }).
-      // make sure prefs updated on the main window
-      then(() => helpers.waitForWindow(app, "Before Dawn: Preferences") ).
-      then(() => app.client.getValue("[name='delay']")).
-      then((res) => {
-        assert.equal("30", res);
+        let updatedPrefs = currentPrefs();
+        assert.equal(30, updatedPrefs.delay);
+        assert.equal(15, updatedPrefs.sleep);
       });
+      // .
+      // // make sure prefs updated on the main window
+      // then(() => helpers.waitForWindow(app, "Before Dawn: Preferences") ).
+      // then(() => app.client.getValue("[name='delay']")).
+      // then((res) => {
+      //   assert.equal("30", res);
+      // });
   });
 
   it("toggles checkboxes", function() {
     let oldConfig = currentPrefs();
 
-    return pickPrefsWindow().
+    return pickSettingsWindow().
       then(() => app.client.waitUntilTextExists("body", "Activate after")).
       then(() => app.client.click("label*=Lock screen after running")).
       then(() => app.client.click("label*=Disable when on battery?")).
       then(() => app.client.click("label*=Auto start on login?")).
       then(() => app.client.click("label*=Only run on the primary display?")).
       then(() => app.client.click("button.save")).
-      then(() => helpers.sleep(100)).
-      // then(() => helpers.waitForWindowClosed(app, "Before Dawn: Settings")).
-      then(function() {
-        assert.equal(!oldConfig.lock, currentPrefs().lock);
-        assert.equal(!oldConfig.disableOnBattery, currentPrefs().disableOnBattery);
-        assert.equal(!oldConfig.auto_start, currentPrefs().auto_start);
-        assert.equal(!oldConfig.runOnSingleDisplay, currentPrefs().runOnSingleDisplay);
+      then(() => helpers.sleep(closeWindowDelay)).
+      then(() => {
+        let updatedPrefs = currentPrefs();
+        assert.equal(!oldConfig.lock, updatedPrefs.lock);
+        assert.equal(!oldConfig.disableOnBattery, updatedPrefs.disableOnBattery);
+        assert.equal(!oldConfig.auto_start, updatedPrefs.auto_start);
+        assert.equal(!oldConfig.runOnSingleDisplay, updatedPrefs.runOnSingleDisplay);
       });
   });
 
   it("leaves checkboxes", function() {
     let oldConfig = currentPrefs();
 
-    return pickPrefsWindow().
+    return pickSettingsWindow().
       then(() => app.client.waitUntilTextExists("body", "Activate after")).
       then(() => app.client.click("button.save")).
-      then(() => helpers.sleep(100)).
-      // then(() => helpers.waitForWindowClosed(app, "Before Dawn: Settings")).
-      then(function() {
-        assert.equal(oldConfig.lock, currentPrefs().lock);
-        assert.equal(oldConfig.disableOnBattery, currentPrefs().disableOnBattery);
-        assert.equal(oldConfig.auto_start, currentPrefs().auto_start);
-        assert.equal(oldConfig.runOnSingleDisplay, currentPrefs().runOnSingleDisplay);
+      then(() => helpers.sleep(closeWindowDelay)).
+      then(() => {
+        let updatedPrefs = currentPrefs();
+        assert.equal(oldConfig.lock, updatedPrefs.lock);
+        assert.equal(oldConfig.disableOnBattery, updatedPrefs.disableOnBattery);
+        assert.equal(oldConfig.auto_start, updatedPrefs.auto_start);
+        assert.equal(oldConfig.runOnSingleDisplay, updatedPrefs.runOnSingleDisplay);
       });
   });
   
   it("allows setting path via dialog", function() {
-    return pickPrefsWindow().
+    return pickSettingsWindow().
       then(() => app.webContents
         .executeJavaScript("document.querySelector(\"button.pick\").scrollIntoView()")).
       then(() => app.client.click("button.pick")).
+      then(() => helpers.sleep(50)).
       then(() => app.client.click("button.save")).
-      then(() => helpers.sleep(100)).
+      then(() => helpers.sleep(closeWindowDelay)).
       then(function() {
         assert.equal("/not/a/real/path", currentPrefs().localSource);
       });
   });
 
   it("clears localSource", function() {
-    return pickPrefsWindow().
+    return pickSettingsWindow().
       then(() => app.client.waitUntilTextExists("body", "Activate after")).
       then(function() {
         let ls = currentPrefs().localSource;
@@ -137,9 +138,9 @@ describe("Settings", function() {
       then(() => app.webContents
         .executeJavaScript("document.querySelector(\"button.clear\").scrollIntoView()")).
       then(() => app.client.click("button.clear")).
+      then(() => helpers.sleep(50)).
       then(() => app.client.click("button.save")).
-      then(() => helpers.sleep(100)).
-      // then(() => helpers.waitForWindowClosed(app, "Before Dawn: Settings")).
+      then(() => helpers.sleep(closeWindowDelay)).
       then(function() {
         assert.equal("", currentPrefs().localSource);
       });
@@ -149,12 +150,13 @@ describe("Settings", function() {
   it("resets defaults", function() {
     const resetDialogOpts = [ { method: "showMessageBox", value: 1 } ];
 
-    return pickPrefsWindow().
+    return pickSettingsWindow().
       then(() => app.fakeDialog.mock(resetDialogOpts)).
       then(() => app.webContents
         .executeJavaScript("document.querySelector(\"button.reset-to-defaults\").scrollIntoView()")).
       then(() => app.client.click("button.reset-to-defaults")).
       then(() => app.client.waitUntilTextExists("body", "Settings reset")).
+      then(() => helpers.sleep(closeWindowDelay)).
       then(function() {
         assert.equal("", currentPrefs().localSource);
       });
