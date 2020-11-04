3cf7e9d732f504249eba604ee3721daddb95be8b
Colin Mitchell <muffinista@gmail.com>
2020-11-04 15:48:39 -0500

    Cleanup eslint, devtools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,12 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: 'babel-eslint',
+    parser: "@babel/eslint-parser",
     ecmaVersion: 2017,
     ecmaFeatures: {
       "spread": true
     },
-    sourceType: 'module'
+    sourceType: "module"
   },
   env: {
     browser: true,
@@ -16,29 +16,29 @@ module.exports = {
   },
   "extends": [
     "eslint:recommended",
-    'plugin:vue/recommended'
+    "plugin:vue/recommended"
   ],
   globals: {
     __static: true
   },
   plugins: [
-    'vue'
+    "vue"
   ],
-  'rules': {
-    'global-require': 0,
-    'import/no-unresolved': 0,
-    'no-param-reassign': 0,
-    'no-shadow': 0,
-    'import/extensions': 0,
-    'import/newline-after-import': 0,
-    'no-multi-assign': 0,
-    'semi': 1,
+  "rules": {
+    "global-require": 0,
+    "import/no-unresolved": 0,
+    "no-param-reassign": 0,
+    "no-shadow": 0,
+    "import/extensions": 0,
+    "import/newline-after-import": 0,
+    "no-multi-assign": 0,
+    "semi": 1,
     "strict": 0,
     "eqeqeq": "off",
     "curly": "warn",
     "quotes": ["warn", "double"],
 
     // allow debugger during development
-    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
+    "no-debugger": process.env.NODE_ENV === "production" ? 2 : 0
   }
-}
+};
