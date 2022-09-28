2824be29736bdcefd1daba9a65f6431476365e07
Colin Mitchell <colin@muffinlabs.com>
2022-09-28 08:41:46 -0400

    Split icons into their own files

---
+++ src/renderer/components/icons/SaveIcon.svelte
@@ -0,0 +1,18 @@
+<svg
+id="Save"
+version="1.1"
+xmlns="http://www.w3.org/2000/svg"
+xmlns:xlink="http://www.w3.org/1999/xlink"
+x="0px"
+y="0px"
+width="14"
+height="14"
+viewBox="0 0 20 20"
+enable-background="new 0 0 20 20"
+xml:space="preserve"
+>
+<path
+  d="M15.173,2H4C2.899,2,2,2.9,2,4v12c0,1.1,0.899,2,2,2h12c1.101,0,2-0.9,2-2V5.127L15.173,2z M14,8c0,0.549-0.45,1-1,1H7
+C6.45,9,6,8.549,6,8V3h8V8z M13,4h-2v4h2V4z"
+/>
+</svg>
